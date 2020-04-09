588e6f44ea8ed37181c4526de50fbaf032f226dd
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-04-09 14:41:24 +0200

    Added mic test if the code works on phone

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+try {
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+}
+catch(e) {
+    console.error(e);
+    $('.no-browser-support').show();
+    $('.app').hide();
+}
+
+recognition.onstart = function() { 
+    instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+    instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+    if(event.error == 'no-speech') {
+        instructions.text('No speech was detected. Try again.');  
+    };
+}
+
+recognition.onresult = function(event) {
+    // event is a SpeechRecognitionEvent object.
+    // It holds all the lines we have captured so far. 
+    // We only need the current one.
+    const output = document.getElementById("output");
+
+    var current = event.resultIndex;
+    output.innerHTML(current);
+    
+    // Get a transcript of what was said.
+    var transcript = event.results[current][0].transcript;
+    alert(transcript);
+  }
+
+  recognition.start();
