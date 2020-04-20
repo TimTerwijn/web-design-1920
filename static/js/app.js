7385b44b3d649363f3235195fc1159514764f21e
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-04-20 15:52:02 +0200

    basic chat setup

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,11 @@
 import * as render from "./modules/Render.js";
+import * as Script from "./modules/Script.js"
 
 function init(){
-    render.vasilisTalks("Lekker tikkie betalen?");
-    render.jennyThinks("static/img/tikkie.png");
+    // render.vasilisTalks("Lekker tikkie betalen?");
+    // render.jennyThinks("static/img/tikkie.png");
 }
 
 init();
+
+
---
+++ static/js/modules/Script.js
@@ -0,0 +1,97 @@
+import * as render from "./Render.js";
+
+{
+    let timestamp = 0.5;
+    timestamp = timestamp * 1000;
+    const message = 
+    "You're listening to The Good, The Bad, and The Interesting,";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 2;
+    timestamp = timestamp * 1000;
+    const message = 
+    "a series of conversations about quality with Vasilis van Gemert and an eclectic mix of designers.";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 7.5;
+    timestamp = timestamp * 1000;
+    const message = 
+    "In this episode I have a conversation with Jenny Shen.";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 13;
+    timestamp = timestamp * 1000;
+    const message = 
+    "Jenny is an independent UX/Product Designer, a public speaker, a meetup organiser,";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 18;
+    timestamp = timestamp * 1000;
+    const message = 
+    "and a mentor for designers, among other things.";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 21;
+    timestamp = timestamp * 1000;
+    const message = "One of the things Jenny specialises in is localisation, so of course we talk about difference in design patterns for different countries.";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 30;
+    timestamp = timestamp * 1000;
+    const message = "We talk about the highly successful Ladies That UX chapter that she started in Amsterdam,";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 36;
+    timestamp = timestamp * 1000;
+    const message = "and we discuss in length why it is so successful, and why it’s needed. Among many other things.";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
+{
+    let timestamp = 43;
+    timestamp = timestamp * 1000;
+    const message = "But as always we start with the question: What makes a thing good?";
+
+    setTimeout(function(){
+        render.vasilisTalks(message)
+    }, timestamp);
+}
+
