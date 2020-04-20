6a0211ab54464cec714540914aed65541b2ca549
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-04-20 13:11:00 +0200

    some progress

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+
+const vasilisTalk = document.getElementById();
+
+
+function vasilisTalks(story){
+    //hide prev talk bubble
+
+    //hide prev think bubble
+
+    //add text to talk bubbel
+
+    //show new talk bubbel
+}
+
+function vasilisThinks(imageUrl){
+    //hide prev talk bubble
+
+    //hide prev think bubble
+
+    //add imgUrl to think image
+
+    //show new think bubbel
+}
+
+function jennyTalks(story){
+    //hide prev talk bubble
+
+    //hide prev think bubble
+
+    //add text to talk bubbel
+
+    //show new talk bubbel
+}
+
+function jennyThinks(imageUrl){
+    //hide prev talk bubble
+
+    //hide prev think bubble
+
+    //add imgUrl to think image
+
+    //show new think bubbel
+}
+
